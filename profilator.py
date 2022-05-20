217662e2c9b4451598fe66dc980b6968390a806c
maciu665 <maciu665@github.com>
2022-05-20 11:48:03 +0200

    Create profilator.py

---
+++ profilator.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import vtk								#to trzeba doinstalowac, "pip install vtk"
+import sys
+import os
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sys.exit()
